d04348009028107998f215ce4de5920b7fdee1b7
Madhav <madhavvv20@gmail.com>
2024-09-17 01:00:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ALOTs
+ ALOTs- One stop solution for all your needs. Whether its cooking, cleaning and ALOTs of other work, We are with you :)
---
+++ src/main/java/com/ALOTs/web/AloTsApplication.java
@@ -0,0 +1,13 @@
+package com.ALOTs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AloTsApplication.class, args);
+	}
+
+}
